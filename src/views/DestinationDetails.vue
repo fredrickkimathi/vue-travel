f292fa684a1827e5007c551d600d77fc311bae25
yourboyfred <fredrickkimathi26@gmail.com>
2024-07-18 12:20:19 +0300

    fetching destination details

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,4 +1,29 @@
 <template>
-    <p>The destination is:{{this.$route.params.id}}</p>
-
+    <section class="destinations">
+        <!-- <h2>{{destination.name}}</h2> -->
+        <div class="destination-details">
+            <img v-if="destination" :src="require(`@/assets/${destination.image}`)" alt="destination.name">
+        </div>
+    </section>
 </template>
+
+<script>
+import store from "@/store.js";
+
+export default {
+    data(){
+        return{
+            destinationId: this.$route.params.id
+        }
+    },
+    
+    computed:{
+        destination(){
+            return store.destinations.find(
+                destination => destination.id == this.destinationId
+            )
+        }
+    }
+}
+
+</script>
