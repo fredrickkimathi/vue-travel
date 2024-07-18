dab0e2a7494750139e852df2ee6b3a717d76db29
yourboyfred <fredrickkimathi26@gmail.com>
2024-07-18 15:34:37 +0300

    destination image and details

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="destinations">
-        <!-- <h2>{{destination.name}}</h2> -->
+        <h2>{{destination.name}}</h2>
         <div class="destination-details">
             <img v-if="destination" :src="require(`@/assets/${destination.image}`)" alt="destination.name">
+            <p>{{destination.description}}</p>
         </div>
     </section>
 </template>
@@ -27,3 +28,21 @@ export default {
 }
 
 </script>
+
+<style scoped>
+img{
+    border-radius:15px;
+    max-width: 500px;
+    width:100%;
+    height: auto;
+    margin: 30px;
+}
+.destination-details{
+    display:flex;
+    justify-content: space-between;
+}
+p{
+    margin: 20px 50px;
+    text-align: justify;
+}
+</style>
