bc3bf308e0c00eb486fa09bc916d7e9addf08be3
yourboyfred <fredrickkimathi26@gmail.com>
2024-07-19 08:12:49 +0300

    navigation component

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,24 +1,61 @@
 <template>
     <nav id="nav">
-        <router-link to="/">Home</router-link>
+      <ul class="nav-links">
+        <li class="links">
+          <router-link to="/">
+            Home
+          </router-link>
+        </li>
+        <li v-for="destination in destinations"
+        :key="destination.name"
+        class="links">
+        <router-link :to="{
+          name: 'DestinationDetails',
+          params: {id:destination.id}
+        }"> 
+        {{ destination.name }}
+
+        </router-link>
+
+        </li>
+      </ul>
+        <!-- <router-link to="/">Home</router-link>
         <router-link to="/brazil">Brazil</router-link>
         <router-link to="/panama">Panama</router-link>
         <router-link to="/hawaii">Hawaii</router-link>
-        <router-link to="/jamaica">Jamaica</router-link>
+        <router-link to="/jamaica">Jamaica</router-link> -->
     </nav>
 </template>
+
+<script>
+import store from "@/store";
+export default{
+  data(){
+    return{
+      destinationId: this.$route.params.id,
+      destinations: store.destinations
+    };
+  }
+}
+</script>
+
 
 
 
 <style scoped>
 #nav{
     display:flex;
-    align-items:center;
-    justify-content:center;
-}
-#nav {
-    padding: 30px;
   }
+.nav-links {
+    display: flex;
+    gap: 10px;
+  }
+.links{
+  text-decoration: none;
+  list-style-type: none;
+
+
+}
   
   #nav a {
     font-weight: bold;
